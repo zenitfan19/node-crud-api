34ffb2932129a3885da068b432e0edce9d9db830
Lavrov, Andrey <alavrov@ru.estee.com>
2024-10-21 18:58:51 +0500

    feat: implement PORT functionality

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
-console.log("Hello, world!");
+import { config as dotenvConfig } from "dotenv";
+import { createServer } from "http";
+
+dotenvConfig();
+
+const PORT = process.env.PORT || 4000;
+
+const server = createServer();
+server.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`));
